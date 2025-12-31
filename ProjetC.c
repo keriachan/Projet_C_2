5118a67eeaa70e6918f3a1f06bb4551050c1488f
Djibril <>
2025-12-31 18:30:07 +0100

    ajout des argument -n -a et -help ajout d'un tri

--- ProjetC.c
+++ ProjetC.c
@@ -68,6 +68,12 @@ void myFree(void* ptr, InfoMem* infoMem, size_t old_size){
 
 // ALGO 1
 
+int comparer_occurrences(const void *a, const void *b) {
+    const Element *elemA = (const Element *)a;
+    const Element *elemB = (const Element *)b;
+    return (elemB->occurrences - elemA->occurrences);
+}
+
 void ajouter_au_dict(Dict *d, const char *mot_lu, InfoMem *info) {
     for (int i = 0; i < d->taille; i++) {
         if (strcmp(d->elem[i].mot, mot_lu) == 0) {
@@ -88,7 +94,7 @@ void ajouter_au_dict(Dict *d, const char *mot_lu, InfoMem *info) {
     d->taille++;
 }
 
-void algo1(FILE *fichier, InfoMem * info) {
+void algo1(FILE *fichier, InfoMem * info, int n) {
     Dict monDict = {NULL, 0, 0};
     Mot courant = {NULL, 0, 0};
     int c;
@@ -115,7 +121,9 @@ void algo1(FILE *fichier, InfoMem * info) {
         courant.mot[courant.taille] = '\0';
         ajouter_au_dict(&monDict, courant.mot, info);
     }
-    for (int i = 0; i < monDict.taille; i++) {
+    qsort(monDict.elem, monDict.taille, sizeof(Element), comparer_occurrences);
+    int max = (n>0 && n<monDict.taille) ? n : monDict.taille;
+    for (int i = 0; i < max; i++) {
         printf("%s : %d\n", monDict.elem[i].mot, monDict.elem[i].occurrences);
         myFree(monDict.elem[i].mot, info, strlen(monDict.elem[i].mot) + 1);
     }
@@ -124,18 +132,55 @@ void algo1(FILE *fichier, InfoMem * info) {
 }
 
 int main(int argc, char *argv[]){
+    int n=-1, a1=0, a2=0;
     for (int i=1; i<argc; i++){
-        if (argv[i][0] != '-'){
-            FILE *fichier = fopen(argv[i], "r");
-            if(fichier != 0){
+        if (strcmp(argv[i], "-help") == 0){
+            printf("Le programme pourra, par exemple, etre lance par la commande :\n./projet [-n int] [-a algo1|algo2|algo3|...] [-help] [-s fichierdesortie] [-l fichierdeperf] fichiers de donnees (plusieurs!)\n");
+        } else if (strcmp(argv[i], "-n") == 0){
+            int resultat = 0;
+            for (int j = 0; argv[i+1][j]; j++) {
+                resultat = (resultat * 10) + (argv[i+1][j] - '0');
+            }
+            n = resultat;
+            i++;
+        } else if (strcmp(argv[i], "-a") == 0){
+            char *copie = strdup(argv[i+1]);
+            char *token = strtok(copie, "|");
+            for (; token; token = strtok(NULL, "|")) {
+                if (strcmp(token, "algo1") == 0) 
+                    a1 = 1;
+                else if (strcmp(token, "algo2") == 0) 
+                    a2 = 1;
+            }
+            free(copie);
+            i++;
+        }
+    }
+    for (int i=1; i<argc; i++){
+        if (strcmp(argv[i], "-n") == 0 || strcmp(argv[i], "-a") == 0) {
+            i++;
+            continue;
+        }
+        if (strcmp(argv[i], "-help") == 0) {
+            continue;
+        }
+        FILE *fichier = fopen(argv[i], "r");
+        if(fichier != 0){
+            InfoMem info = {0, 0, 0};
+            if (a1){
                 InfoMem info = {0, 0, 0};
-                algo1(fichier, &info);
+                algo1(fichier, &info, n);
+                printf("Algorithme 1 :\n");
                 printf("Cumul Alloc: %zu\n", info.cumul_alloc);
                 printf("Cumul Desalloc: %zu\n", info.cumul_desalloc);
                 printf("Pic (Max Alloc): %zu\n", info.max_alloc);
             }
-            fclose(fichier);
+            if (a2)
+                printf("Pas encore de deuxieme algo.\n");
+        } else {
+            printf("Le fichier %s ne peu pas etre lu.\n", argv[i]);
         }
+        fclose(fichier);
     }
     return 0;
 }
